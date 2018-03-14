58fe24307bd966ad4a9e7234dedcc267c3d03eec
Boris Wolfson <boris.wolfson@tomtom.com>
2018-03-14 18:45:06 +0100

    cycle derivation

--- bellmanFord.m
+++ bellmanFord.m
@@ -1,8 +1,9 @@
-function [distance, predecessor, newPredecessor] = bellmanFord()
+function [distance, predecessor, cycleNodes] = bellmanFord()
 %Belman-Ford
 iu = 1;
 iv = 2;
 iw = 3;
+
 W = [1, 2, 4;
     1, 4, 4;
     3, 1, 3;
@@ -20,29 +21,46 @@ W = [1, 2, 4;
 nodesNumber = max(W(:, iu));
 edgesNumber = length(W);
 predecessor = 0*ones(nodesNumber,1);
-
 distance = inf*ones(nodesNumber, 1);
 distance(1) = 0;
+cycleNodes          = zeros(nodesNumber, 1);
+nodeIndexes     = zeros(nodesNumber, 1);
+isCycleNotDefined   = true;
+isCycleNotFnd   = true;
+
 for node = 1:nodesNumber-1
     prevDistance = distance;
     for edge = 1:edgesNumber
         u = W(edge, iu);
         v = W(edge, iv);
         w = W(edge, iw);
-        [isRelaxed, distance, predecessor] = relax( u, v, w, distance, prevDistance, predecessor);
+        [~, distance, predecessor] = relax( u, v, w, distance, prevDistance, predecessor);
     end
 end
 
 prevDistance = distance;
-newPredecessor = predecessor;
 for edge = 1:edgesNumber
     u = W(edge, iu);
     v = W(edge, iv);
     w = W(edge, iw);
-    [isRelaxed, distance, newPredecessor] = relax( u, v, w, distance, prevDistance, predecessor);
+    [isRelaxed, distance, ~] = relax( u, v, w, distance, prevDistance, predecessor);
     if isRelaxed
         sprintf('negative cycle detection on node %d', v)
+        % find first cycle index
+        nodeIndexes(v)  = v;
+        while isCycleNotFnd
+            v               = predecessor(v);
+            isCycleNotFnd   = ~nodeIndexes(v);
+            nodeIndexes(v)  = v;
+        end
         
+        cycleNodes(v)       = v; % first cycle node
+        while isCycleNotDefined
+            v                   = predecessor(v);
+            isCycleNotDefined   = ~cycleNodes(v);
+            cycleNodes(v)       = v;
+        end
+        break
     end
 end
 
